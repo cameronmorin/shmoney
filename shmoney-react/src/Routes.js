75eeaeeca96429d078576ab69d7e46686438a9aa
Cameron <cmori007@ucr.edu>
2019-10-28 12:21:40 -0700

    Create Home page container

--- shmoney-react/src/Routes.js
+++ shmoney-react/src/Routes.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 // import all routes here
 import AppliedRoute from './components/util/AppliedRoute';
-import Header from './components/Header';
+import Home from './containers/Home';
 import NotFound from './containers/NotFound';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
 
 export default ({ childProps }) =>
   <Switch>
-    <AppliedRoute path="/" exact component={Header} props={childProps} />
+    <AppliedRoute path="/" exact component={Home} props={childProps} />
     <AppliedRoute path="/signIn" exact component={SignIn} props={childProps} />
     <AppliedRoute path="/signUp" exact component={SignUp} props={childProps} />
     {/* Finally, catch all unmounted routes */}
---
+++ shmoney-react/src/containers/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>I am home page</h1>
+      </>
+    );
+  }
+}
