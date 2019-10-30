fae297e18851fd600a8133dfef6f7fe93de756da
Cameron <cmori007@ucr.edu>
2019-10-29 23:50:14 -0700

    Update NoPath styling

--- shmoney-react/src/components/NoPath.js
+++ shmoney-react/src/components/NoPath.js
@@ -8,32 +8,22 @@ import { Link, withRouter } from 'react-router-dom'
 export default class NoPath extends React.Component {  
   render() {
     return (
-        <div className = "PageWrap" style = {{height: '100vh'}}>
-            <div className = "TempNavBar">
-
+        <div className="page">
+					<div className="title-text">
+            Welcome to $hmoney, the new and easy way to split payments between friends
+          </div>
+          <div className="home-buttons">
+            <div className="sign-in-button">
+              <div className="button" onClick={this.props.onClickButton}>
+                Sign In
+              </div> 
             </div>
-            <div className = "HomeBody" >
-                <div className = "Msg">
-                    Welcome to $hmoney, the new and easy way to split payments between friends
-                </div>
+            <div className="sign-up-button">
+              <div className="button" onClick={this.props.onClickButton}>
+                Sign Up
+              </div>
             </div>
-            <div className = "HomeButtons">
-                <div className = "SignInButton">
-                    <div className = "Button">
-                        <Link to='/signin'>Sign In</Link>
-                    </div> 
-                </div>
-                <div className = "OrDisplay">
-                    <div className = "Msg">
-                        Or
-                    </div>
-                </div>
-                <div className = "SignUpButton:">
-                    <div className = "Button">
-                        <Link to='/signup'>Sign Up</Link>
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
     )
   }
