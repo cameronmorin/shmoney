49b5ac68dded06a248344921c36aca7282acb6ce
cpivo001 <cpivo001@ucr.edu>
2019-10-29 20:40:33 -0700

    forgot to revert home page path before pushing my branch

--- shmoney-react/src/Routes.js
+++ shmoney-react/src/Routes.js
@@ -8,11 +8,12 @@ import NotFound from './containers/NotFound';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
 import NoPath from './components/NoPath';
+import Home from './components/Home';
 
 export default ({ childProps }) =>
   <Switch>
     <AppliedRoute path="/" exact component={NoPath} props={childProps} />
-    <AppliedRoute path="/home" exact component={NoPath} props={childProps} />
+    <AppliedRoute path="/home" exact component={Home} props={childProps} />
     <AppliedRoute path="/signIn" exact component={SignIn} props={childProps} />
     <AppliedRoute path="/signUp" exact component={SignUp} props={childProps} />
     <AppliedRoute path="/profile" exact component={Profile} props={childProps} />
