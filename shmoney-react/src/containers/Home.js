ee4f261f4cb60ac708a085bfa55cb43f5f69013b
Cameron <cmori007@ucr.edu>
2019-10-29 22:33:38 -0700

    Render NavBar component in Home container

--- shmoney-react/src/containers/Home.js
+++ shmoney-react/src/containers/Home.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import NavBar from '../components/NavBar';
 
 export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <>
-        <h1>I am home page</h1>
+      {this.props.authUser != null &&
+        <NavBar 
+        authUser={this.props.authUser}
+        onLoad={this.props.onLoad}
+        onClickHome={this.props.onClickHome}
+        onClickLogout={this.props.onClickLogout}
+        onClickAvatar={this.props.onClickAvatar}
+        displayName="toBeFixed"
+        />
+      }
+        {/* HomePage component */}
+        <h1>Home Page</h1>
       </>
     );
   }
