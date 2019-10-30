a263cd28a7c7eda608eae50edb4f6075ae72d1b2
Cameron <cmori007@ucr.edu>
2019-10-29 22:32:51 -0700

    Create menu functions in App and pass as props

--- shmoney-react/src/App.js
+++ shmoney-react/src/App.js
@@ -6,11 +6,38 @@ import { compose } from 'recompose';
 import Routes from './Routes';
 
 class App extends React.Component {
+  onClickHome = async event => {
+    event.preventDefault();
+    
+    if (window.location.pathname != "/") {
+      this.props.history.push("/");
+    }
+  }
+
+  onClickLogout = async event => {
+    event.preventDefault();
+
+    this.props.firebase.logout();
+  }
+
+  onClickAvatar = async event => {
+    event.preventDefault();
+
+    // const userPath = '/' + authUser.displayName;
+    if (window.location.pathname != '/user') {
+      // this.props.history.push(userPath);
+      this.props.history.push('/profile');
+    }
+  }
+
   render() {
     this.childProps = {
       currPage: 'Home',
       message: 'Hi',
       authUser: null,
+      onClickHome: this.onClickHome,
+      onClickLogout: this.onClickLogout,
+      onClickAvatar: this.onClickAvatar
     }
 
     return(
