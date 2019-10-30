059780092fd7496420672648b662efb5b01ea52a
cpivo001 <cpivo001@ucr.edu>
2019-10-29 20:34:30 -0700

    created ui for page with no path. There is a welcome msg with a very briefexplanation of app. There are also two buttons leading to the sign in and sign up page

--- shmoney-react/src/Routes.js
+++ shmoney-react/src/Routes.js
@@ -7,12 +7,12 @@ import Profile from './containers/Profile';
 import NotFound from './containers/NotFound';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
-import HomePage from './components/HomePage';
+import NoPath from './components/NoPath';
 
 export default ({ childProps }) =>
   <Switch>
-    <AppliedRoute path="/" exact component={Header} props={childProps} />
-    <AppliedRoute path="/home" exact component={HomePage} props={childProps} />
+    <AppliedRoute path="/" exact component={NoPath} props={childProps} />
+    <AppliedRoute path="/home" exact component={NoPath} props={childProps} />
     <AppliedRoute path="/signIn" exact component={SignIn} props={childProps} />
     <AppliedRoute path="/signUp" exact component={SignUp} props={childProps} />
     <AppliedRoute path="/profile" exact component={Profile} props={childProps} />
