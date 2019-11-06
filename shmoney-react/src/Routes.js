60aaa8825d7ba5707e48c76a80ab4bb7acb20832
Christopher Gentibano <cgent003@ucr.edu>
2019-11-06 13:54:50 -0800

    Adding in MyGroup page

--- shmoney-react/src/Routes.js
+++ shmoney-react/src/Routes.js
@@ -8,6 +8,7 @@ import Profile from './containers/Profile';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
 import UploadImage from './components/UploadImage';
+import MyGroup from './containers/MyGroup';
 
 export default ({ childProps }) =>
   <Switch>
@@ -15,6 +16,7 @@ export default ({ childProps }) =>
     <AppliedRoute path="/signIn" exact component={SignIn} props={childProps} />
     <AppliedRoute path="/signUp" exact component={SignUp} props={childProps} />
     <AppliedRoute path="/profile" exact component={Profile} props={childProps} />
+    <AppliedRoute path="/mygroup" exact component={MyGroup} props={childProps} />
     <AppliedRoute path="/updateProfilePicture" exact component={UploadImage} props={childProps} />
     {/* Finally, catch all unmounted routes */}
     <Route component={NotFound} props={childProps}/>
