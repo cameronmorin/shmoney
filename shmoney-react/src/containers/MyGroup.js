75612257e5b59efb81cfab08771acf17f540e8da
Christopher Gentibano <cgent003@ucr.edu>
2019-11-14 17:19:41 -0800

    Added in modals using bootstrap for buttons in MyPage

--- shmoney-react/src/containers/MyGroup.js
+++ shmoney-react/src/containers/MyGroup.js
@@ -1,14 +1,166 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+
 import NavBar from '../components/NavBar';
 import "../styles/MyGroup.css"
 
+import { Bootstrap, Button, Modal, InputGroup, FormControl, ListGroup } from 'react-bootstrap';
+
+
 import { withAuthorization, withAuthUserContext } from '../components/session'
 
+const AddMembers = () => {
+    const [show, setShow] = useState(false);
 
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <>
+            <Button variant="secondary" onClick={handleShow}>
+                Add Members
+        </Button>
+
+            <Modal show={show} onHide={handleClose} animation={false}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Add Members</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <InputGroup className="mb-3">
+                        <FormControl
+                            placeholder="Recipient's username"
+                            aria-label="Recipient's username"
+                            aria-describedby="basic-addon2"
+                        />
+                        <InputGroup.Append>
+                            <Button variant="outline-secondary">Add</Button>
+                        </InputGroup.Append>
+                    </InputGroup>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>Close</Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
+
+const AddBill = () => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <>
+            <Button variant="secondary" onClick={handleShow}>
+                Add Bills
+        </Button>
+            <Modal show={show} onHide={handleClose} animation={false}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Add Bills</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Prepend>
+                            <InputGroup.Text>$</InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <FormControl aria-label="Amount (to the nearest dollar)" />
+                        <InputGroup.Append>
+                            <InputGroup.Text>.00</InputGroup.Text>
+                        </InputGroup.Append>
+                    </InputGroup>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Add
+                    </Button>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
+
+const DeleteMembers = () => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <>
+            <Button variant="secondary" onClick={handleShow}>
+                Delete Members
+        </Button>
+
+            <Modal show={show} onHide={handleClose} animation={false}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Delete Members</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <InputGroup className="mb-3">
+                        <FormControl
+                            placeholder="Recipient's username"
+                            aria-label="Recipient's username"
+                            aria-describedby="basic-addon2"
+                        />
+                        <InputGroup.Append>
+                            <Button variant="outline-secondary">Delete</Button>
+                        </InputGroup.Append>
+                    </InputGroup>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                     </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
+
+const ViewLedger = () => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <>
+            <Button variant="secondary" onClick={handleShow}>
+                View Ledger
+        </Button>
+
+            <Modal show={show} onHide={handleClose} animation={false}>
+                <Modal.Header closeButton>
+                    <Modal.Title>View Ledger</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <ListGroup>
+                        <ListGroup.Item variant="danger">Late - 01/01/2019 - $2000</ListGroup.Item>
+                        <ListGroup.Item>On Time - 02/01/2019 - $1000</ListGroup.Item>
+                        <ListGroup.Item>On Time - 03/01/2019 - $1000</ListGroup.Item>
+                        <ListGroup.Item>On Time - 04/01/2019 - $1000</ListGroup.Item>
+                        <ListGroup.Item variant="warning">On Time Overpaid - 05/01/2019 - $1005</ListGroup.Item>
+                    </ListGroup>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+            </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
 
 class MyGroup extends React.Component {
+
     render() {
-        const authUser = this.props.authUser;
+
         return (
             <div>
                 <NavBar
@@ -21,10 +173,10 @@ class MyGroup extends React.Component {
                 <div className="main-grid">
                     <div className="left-grid">
                         <h1>House Name</h1>
-                        <button className="btn bill">Add bill</button>
-                        <button className="btn add">Add members</button>
-                        <button className="btn del">Delete members</button>
-                        <button className="btn ledger">View ledger</button>
+                        <AddBill />
+                        <AddMembers />
+                        <DeleteMembers />
+                        <ViewLedger />
                     </div>
 
                     <div className="right-grid">
@@ -35,12 +187,17 @@ class MyGroup extends React.Component {
                         <h1>Payment History </h1>
                         <p> [list payments] </p>
                     </div>
+
                 </div>
             </div>
+
         );
     }
 }
+
+
 const signedInRoute = true;
 const myGroup = withAuthUserContext(MyGroup);
 
 export default withAuthorization(signedInRoute)(myGroup)
+
