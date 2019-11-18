dea3861549324d90c3fb1c8334759bd710ebe586
Cameron <cmori007@ucr.edu>
2019-11-18 01:08:36 -0800

    Refactor NavBar component to be responsive

--- shmoney-react/src/components/NavBar.js
+++ shmoney-react/src/components/NavBar.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import { AuthUserContext } from './session'
 
-import NavBar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
-import logo from '../images/logo.png';
+import logo from '../images/box_logo.png';
 import avatar from '../images/avatar.svg';
 import '../styles/NavBar.css';
 
@@ -15,47 +13,37 @@ export default class CustomNavBar extends React.Component {
   }
   
   render() {
-    const currPage = this.props.currPage;
     return(
       <>
-        <nav bg="light" expand="lg">
-          <NavBar.Brand className="brand"><img className="logo" src={logo} alt="logo" /></NavBar.Brand>
-          <Nav className="mr-auto nav-links">
-            <Nav.Item className={'on-hover ' + ((currPage === 'Group')? 'active-link' : 'link')} onClick={this.props.onClickMenu}>Group</Nav.Item>
-            <Nav.Item className={'on-hover ' + ((currPage === 'Rent')? 'active-link' : 'link')}>Rent</Nav.Item>
-          </Nav>
-        </nav>
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+          <Navbar.Brand>
+            <img className="logo" src={logo} alt="logo" onClick={this.props.onClickHome} />
+          </Navbar.Brand>
+          <AuthUserContext.Consumer>
+            {authUser => authUser ?
+              <>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                  <Nav className="mr-auto">
+                    <Nav.Link onClick={this.props.onClickMenu}>Group</Nav.Link>
+                    {/* <Nav.Link className={((this.props.currPage === 'Rent')? 'active-link' : '')} href="/rent">Rent</Nav.Link> */}
+                  </Nav>
+                  <Nav>
+                  <div className="nav-user">
+                    {/* insert user info here if signed in, else signIn/Up links */}
+                    <div className="user-msg">
+                      <div className="welcome-msg">{authUser.displayName}</div>
+                      <div className="logout-msg" onClick={this.props.onClickLogout}>Logout</div>
+                    </div>
+                    <img className="avatar" src={authUser.photoURL ? authUser.photoURL : avatar} alt="user" onClick={this.props.onClickAvatar} />
+                  </div>
+                  </Nav>
+                </Navbar.Collapse>
+              </> : null
+            }
+          </AuthUserContext.Consumer>
+        </Navbar>
       </>
     );
   }
 }
-
-// <div className="nav-grid">
-//           <div className="nav-logo">
-//             {/* display logo here */}
-//             <img className="logo" src={logo} alt="logo" onClick={this.props.onClickHome}/>
-//           </div>
-//           <>
-//             {/* Check authUser status and render nav-pages and nav-user if they are logged in
-//             Can change null to whatever needs to be rendered when authUser is logged out */}
-//             <AuthUserContext.Consumer>
-//             {authUser => authUser ? 
-//               <>
-//                 <div className="nav-pages">
-//                   {/* insert page links here */}
-//                   <NavLink to="/group" className="link" activeClassName="active-link">My Group</NavLink>
-//                   <NavLink to="/myrent" className="link" activeClassName="active-link">Pay Rent</NavLink>
-//                 </div>
-//                 <div className="nav-user">
-//                   {/* insert user info here if signed in, else signIn/Up links */}
-//                   <div className="user-msg">
-//                     <div className="welcome-msg">{authUser.displayName}</div>
-//                     <div className="logout-msg" onClick={this.props.onClickLogout}>Logout</div>
-//                   </div>
-//                   <img className="avatar" src={authUser.photoURL ? authUser.photoURL : avatar} alt="user" onClick={this.props.onClickAvatar} />
-//                 </div>
-//               </> : null
-//             }
-//             </AuthUserContext.Consumer>
-//           </>
-//         </div>
