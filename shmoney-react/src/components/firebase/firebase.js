60bf38a826599b9e3e1b99fc16643ad7c4863fe0
Cameron <cmori007@ucr.edu>
2019-11-20 10:52:44 -0800

    Move API key to hidden file

--- shmoney-react/src/components/firebase/firebase.js
+++ shmoney-react/src/components/firebase/firebase.js
@@ -2,17 +2,7 @@ import app from 'firebase/app'
 import 'firebase'
 import 'firebase/auth'
 import 'firebase/firestore';
-
-const firebaseConfig = {
-	apiKey: "AIzaSyBmOBXj6X0pPNLnIZKBaV_RszLpmy5fXas",
-	authDomain: "shmoney-617ec.firebaseapp.com",
-	databaseURL: "https://shmoney-617ec.firebaseio.com",
-	projectId: "shmoney-617ec",
-	storageBucket: "shmoney-617ec.appspot.com",
-	messagingSenderId: "123891123896",
-	appId: "1:123891123896:web:9a17a4c90e12c452c7964d",
-	measurementId: "G-70DJ7C1469"
-};
+import firebaseConfig from './api';
 
 class Firebase {
 	constructor() {
