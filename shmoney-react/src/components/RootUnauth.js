8c5b24e9d08d02430a6f6d1e756e39ccda6a10ef
Alex Nguyen <43830043+anguy258@users.noreply.github.com>
2019-11-20 09:22:48 -0800

    Started Refactoring Unauthorized Home Page

--- shmoney-react/src/components/RootUnauth.js
+++ shmoney-react/src/components/RootUnauth.js
@@ -11,9 +11,19 @@ export default class NoPath extends React.Component {
           <AuthUserContext.Consumer>
             {authUser => authUser ? <Dashboard/> :
               <div className="page">
-                <div className = "UnAuth">
-                  <div className="title-text">
-                    Welcome to $hmoney, the new and easy way to split payments between friends
+                <div className = "top-box">
+                  <div className="subject-text top">
+                    <p>
+                      $hmoney helps you <br></br>
+                      keep track of your rent <br></br>
+                      and stay organized.
+                    </p>
+                  </div>
+                  <div className = "description-text top">
+                    Form groups and divide rent among <br></br>
+                    roommates to stay on track with <br></br>
+                    due dates in an effective and <br></br>
+                    efficient manner.
                   </div>
                   <div className="home-buttons">
                     <div className="sign-in-button">
@@ -21,12 +31,28 @@ export default class NoPath extends React.Component {
                         Sign In
                       </div> 
                     </div>
-                    <div className="sign-up-button">
+                    <div className="get-started-button">
                       <div className="button" onClick={this.props.onClickButton}>
-                        Sign Up
+                        Get Started
                       </div>
                     </div>
                   </div>
+                </div>
+                <div className = "subject-text bottom">
+                  Organize into groups
+                </div>
+                <div className = "description-text bottom">
+                  Find and add other Shmoney members <br></br>
+                  to your group to divide up the rent <br></br>
+                  amongst group members.
+                </div>
+                <div className = "subject-text-right">
+                  Information at a glance
+                </div>
+                <div className = "description-text-right">
+                  Access data regarding your group such as <br></br>
+                  who has/hasn't paid rent, due dates, previous<br></br>
+                  bill history, and other statistical data.
                 </div>
               </div>
             }
