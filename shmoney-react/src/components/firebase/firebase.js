3c74f87c3540cfe81b0cbe502c2f485aff605065
Kelton <kadey001@ucr.edu>
2019-11-22 17:41:09 -0800

    Moved firebase config to ignored file and updated API key

--- shmoney-react/src/components/firebase/firebase.js
+++ shmoney-react/src/components/firebase/firebase.js
@@ -2,16 +2,17 @@ import app from 'firebase/app'
 import 'firebase'
 import 'firebase/auth'
 import 'firebase/firestore';
+import config from '../../config'
 
 const firebaseConfig = {
-	apiKey: "AIzaSyBmOBXj6X0pPNLnIZKBaV_RszLpmy5fXas",
-	authDomain: "shmoney-617ec.firebaseapp.com",
-	databaseURL: "https://shmoney-617ec.firebaseio.com",
-	projectId: "shmoney-617ec",
-	storageBucket: "shmoney-617ec.appspot.com",
-	messagingSenderId: "123891123896",
-	appId: "1:123891123896:web:9a17a4c90e12c452c7964d",
-	measurementId: "G-70DJ7C1469"
+	apiKey: config.apiKey,
+	authDomain: config.authDomain,
+	databaseURL: config.databaseURL,
+	projectId: config.projectId,
+	storageBucket: config.storageBucket,
+	messagingSenderId: config.measurementId,
+	appId: config.appId,
+	measurementId: config.measurementId,
 };
 
 class Firebase {
